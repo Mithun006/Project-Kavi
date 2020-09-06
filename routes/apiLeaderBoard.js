17bfadec3039f8f4c430bd9c342f8a1c72b44831
Mithun006 <48342777+Mithun006@users.noreply.github.com>
2020-09-06 20:39:45 +0530

    Add files via upload

--- routes/apiLeaderBoard.js
+++ routes/apiLeaderBoard.js
@@ -6,10 +6,12 @@ const Student = require("../models/student");
 
 // Get leaderboard of a specific College (Requires: collegeId)
 routerLeader.get("", checkAuth, function(req,res,next){
-    Student.find({collegeId: req.body.collegeId},{overallScore: 1, firstName: 1, lastName: 1}).then(studentsList => {
-        console.log(studentsList.sort().reverse())
-        res.status(201).json(studentsList.sort().reverse());
-    })
+    Student.find({collegeId: req.body.collegeId},{overallScore: 1, firstName: 1, lastName: 1})
+        .sort({overallScore: -1})
+        .then(studentsList =>{
+            console.log(studentsList)
+            res.status(201).json(studentsList)
+        })
 })
 
 module.exports = routerLeader;
