665399a746e3b284c8ff2b205bf61d6f063af6a5
Mithun006 <48342777+Mithun006@users.noreply.github.com>
2020-09-07 19:12:39 +0530

    Add files via upload

--- routes/apiLeaderBoard.js
+++ routes/apiLeaderBoard.js
@@ -2,16 +2,59 @@ const express = require("express");
 const routerLeader = express.Router();
 const checkAuth = require("../middleware/check-auth");
 
-const Student = require("../models/student");
+const Student = require("../models/student")
 
 // Get leaderboard of a specific College (Requires: collegeId)
-routerLeader.get("", checkAuth, function(req,res,next){
-    Student.find({collegeId: req.body.collegeId},{overallScore: 1, firstName: 1, lastName: 1})
+routerLeader.get('', checkAuth, function(req,res,next){
+    Student.find({collegeId: req.body.collegeId}, {overallScore: 1, firstName: 1, lastName: 1})
         .sort({overallScore: -1})
         .then(studentsList =>{
             console.log(studentsList)
             res.status(201).json(studentsList)
         })
 })
+
+// Get Ranksheet for a specific cTest (Requires: _id(cTest))
+routerLeader.get('/rankSheet/cTest', checkAuth, function(req,res,next){
+    Student.find({collegeId: req.body.collegeId}, {_id: 1, firstName: 1, lastName: 1}).populate('testScore').then(studentsList =>{
+        let rankSheet = new Array()
+        studentsList.forEach(value =>{
+            value.testScore.cTest.forEach(element => {
+                if(element.testId.equals(req.body.cTest)){
+                    rankSheet.push({
+                        score: element.score,
+                        studentId: value._id,
+                        firstName: value.firstName,
+                        lastName: value.lastName
+                    })
+                }
+            });
+        });
+        console.log(rankSheet.sort(function(a,b){return a.score - b.score}).reverse())
+        res.status(201).json(rankSheet.sort(function(a,b){return a.score - b.score}).reverse())
+    })            
+});
+
+// Get Ranksheet for a specific cTest (Requires: _id(cTest))
+routerLeader.get('/rankSheet/aTest', checkAuth, function(req,res,next){
+    Student.find({collegeId: req.body.collegeId}, {_id: 1, firstName: 1, lastName: 1}).populate('testScore').then(studentsList =>{
+        let rankSheet = new Array()
+        studentsList.forEach(value =>{
+            value.testScore.aTest.forEach(element => {
+                if(element.testId.equals(req.body.aTest)){
+                    rankSheet.push({
+                        score: element.score,
+                        studentId: value._id,
+                        firstName: value.firstName,
+                        lastName: value.lastName
+                    })
+                }
+            });
+        });
+        console.log(rankSheet.sort(function(a,b){return a.score - b.score}).reverse())
+        res.status(201).json(rankSheet.sort(function(a,b){return a.score - b.score}).reverse())
+    })            
+});
+
 
 module.exports = routerLeader;
