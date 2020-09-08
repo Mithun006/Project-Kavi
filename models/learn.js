30beba134722e58d6f3f9e7c7a67aa8999febb34
Mithun006 <48342777+Mithun006@users.noreply.github.com>
2020-09-08 17:37:08 +0530

    Add files via upload

---
+++ models/learn.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create Learn Schema & Models
+const LearnSchema = new Schema({
+    introduction: [ String ],
+    keyPoints: {
+        brief: String,
+        points:[ String ]
+    },
+    types: {
+        brief: String,
+        types: [{
+            brief: String,
+            typesOfType: [String]
+        }],
+    },
+    formula: [{
+        type: String,
+        formula: String,
+        explain: String
+    }],
+    explanation: [{
+        type: String,
+        brief: String,
+        formula: String,
+        example: [{
+            question: String,
+            solution: [{
+                formula: String,
+                solution: String
+            }],
+            shortcut: String
+        }]
+    }]
+})
+
+
+const Learn = mongoose.model('Learn',LearnSchema);
+module.exports = Learn
