ddb80276ef628e4fad13ae9293f8743e7362ba06
Mithun006 <48342777+Mithun006@users.noreply.github.com>
2020-09-08 18:35:00 +0530

    Update learn.js

--- models/learn.js
+++ models/learn.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 // create Learn Schema & Models
 const LearnSchema = new Schema({
+    topic: {type: mongoose.Schema.Types.ObjectId, ref: "Topic"},
     introduction: [ String ],
     keyPoints: {
         brief: String,
